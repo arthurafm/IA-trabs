90ee21f0815ac990f7576d315fd38ebdb330a028
Arthur <arthuralvesfmelo@gmail.com>
2023-06-21 16:54:30 -0300

    fit feita

--- alegrete.py
+++ alegrete.py
@@ -24,18 +24,12 @@ def step_gradient(b, w, data, alpha):
 
 
 def fit(data, b, w, alpha, num_iterations):
-    """
-    Para cada época/iteração, executa uma atualização por descida de
-    gradiente e registra os valores atualizados de b e w.
-    Ao final, retorna duas listas, uma com os b e outra com os w
-    obtidos ao longo da execução (o último valor das listas deve
-    corresponder à última época/iteração).
-
-    :param data: np.array - matriz com o conjunto de dados, x na coluna 0 e y na coluna 1
-    :param b: float - bias (intercepto da reta)
-    :param w: float - peso (inclinacao da reta)
-    :param alpha: float - taxa de aprendizado (a.k.a. tamanho do passo)
-    :param num_iterations: int - numero de épocas/iterações para executar a descida de gradiente
-    :return: list,list - uma lista com os b e outra com os w obtidos ao longo da execução
-    """
-    raise NotImplementedError  # substituir pelo seu codigo
+    new_b = b
+    new_w = w
+    list_b = []
+    list_w = []
+    for i in range(num_iterations):
+        new_b, new_w = step_gradient(new_b, new_w, data, alpha)
+        list_b.append(new_b)
+        list_w.append(new_w)
+    return list_b, list_w
