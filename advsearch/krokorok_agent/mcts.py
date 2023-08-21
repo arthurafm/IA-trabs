c2650ee15413c11238d307f1b478f5a17a23b634
NegrY <48329860+0NegrY0@users.noreply.github.com>
2023-08-21 10:05:11 -0300

    algoritimo monte carlo parece certo, falta a chamada

--- advsearch/krokorok_agent/mcts.py
+++ advsearch/krokorok_agent/mcts.py
@@ -1,6 +1,71 @@
 import random
 from typing import Tuple
 
+class Node:
+
+    exploration_coef = 0.7
+
+    def __init__(self, state, father):
+        self.sons = []
+        self.wins = 0
+        self.games = 0
+        self.state = state
+        self.father = father
+
+
+    def successors(s) -> Set:
+        successorsSet = set()
+
+        for move in s.legal_moves():
+            successorsSet.add((s.next_state(move), move))
+
+        return successorsSet
+
+    def evaluate(self):
+        if self.state is None:
+            return float('-inf')
+        elif self.games == 0:
+            return float('inf')
+        else:
+            return (self.wins / self.games) + 2*exploration_coef*sqrt((2*math.log(self.father.games))/self.games)
+
+
+    def selection(node):
+        while(node.sons != []):
+            prefered_son = Node(None, None)
+            for newNode in node.sons:
+                if newNode.evaluate() > prefered_son.evaluate():
+                    prefered_son = newNode
+            node = prefered_son
+        return node
+    
+    
+    def expansion(self, state):
+        newNode = Node(state, self)
+        self.sons.push(newNode)
+
+
+    def simulation(self, state, player):
+        while(!state.is_terminal()):
+            state = random.choice(successors(state))
+        if state.winner == player:
+            return 1
+        elif state.winner == None:
+            return 0.5
+        else:
+            return 0
+
+
+    def retropropag(node, result):
+        while (node.father != None):
+            node.wins += result
+            node.games += 1
+            node = node.father
+     
+
+
+
+
 def make_move(state) -> Tuple[int, int]:
     """
     Returns a move for the given game state. 
@@ -10,55 +75,6 @@ def make_move(state) -> Tuple[int, int]:
     :param state: state to make the move
     :return: (int, int) tuple with x, y coordinates of the move (remember: 0 is the first row/column)
     """
-
-    # o codigo abaixo retorna uma jogada ilegal
-    # Remova-o e coloque a sua implementacao do MCTS
-
-class Node:
-
-    def __init__(self):
-        self.sons = []
-        self.wins = 0
-        self.games = 0
-        self.state = None
-
-
-    def evaluate(self, state):
-
-    
-    def successors(s) -> Set:
-        successorsSet = set()
-        for move in s.legal_moves():
-            successorsSet.add((s.next_state(move), move))
-        return successorsSet
-
-
-    def selection(self, state):
-        while(!state.is_terminal()):
-            prefered_son = None
-            for(newS, newA) in successors(state):
-                if self.evaluate(newS) > self.evaluate(prefered_son):
-                    prefered_son = newS
-            state = prefered_son
-        return state
-    
-    
-    def expansion:
-
-
-    def simulation(self, state, player):
-        while(!state.is_terminal()):
-            state = random.choice(successors(state))
-        if state.winner == player:
-            return 1
-        else:
-            return 0
-
-
-    def retropropag:
-    
-     
-
 
 
 
