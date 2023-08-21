dd09b2c3fea0a9ef13e2fc7bcce9c420df44c581
NegrY <48329860+0NegrY0@users.noreply.github.com>
2023-08-21 09:30:10 -0300

    fazendo mcts

--- advsearch/krokorok_agent/mcts.py
+++ advsearch/krokorok_agent/mcts.py
@@ -14,6 +14,51 @@ def make_move(state) -> Tuple[int, int]:
     # o codigo abaixo retorna uma jogada ilegal
     # Remova-o e coloque a sua implementacao do MCTS
 
-    return (-1, -1)
+class Node:
+
+    def __init__(self):
+        self.sons = []
+        self.wins = 0
+        self.games = 0
+        self.state = None
+
+
+    def evaluate(self, state):
+
+    
+    def successors(s) -> Set:
+        successorsSet = set()
+        for move in s.legal_moves():
+            successorsSet.add((s.next_state(move), move))
+        return successorsSet
+
+
+    def selection(self, state):
+        while(!state.is_terminal()):
+            prefered_son = None
+            for(newS, newA) in successors(state):
+                if self.evaluate(newS) > self.evaluate(prefered_son):
+                    prefered_son = newS
+            state = prefered_son
+        return state
+    
+    
+    def expansion:
+
+
+    def simulation(self, state, player):
+        while(!state.is_terminal()):
+            state = random.choice(successors(state))
+        if state.winner == player:
+            return 1
+        else:
+            return 0
+
+
+    def retropropag:
+    
+     
+
+
 
 
